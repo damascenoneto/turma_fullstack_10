aa379f80ffa87b3a42cdc3b0ec3656385ac9fb7d
damascenoneto <cddndamasceno@gmail.com>
2026-03-04 21:40:37 -0300

    Endpoints de crud de usuarios

---
+++ sgp-api/src/main/java/br/com/treina/recife/sgp/api/controller/ProjetoController.java
@@ -0,0 +1,36 @@
+package br.com.treina.recife.sgp.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.treina.recife.sgp.api.model.Projeto;
+import br.com.treina.recife.sgp.api.service.ProjetoService;
+
+@RestController
+@RequestMapping("/api/projetos")
+public class ProjetoController {
+    
+
+    @Autowired
+    private ProjetoService projetoService;
+
+    @PostMapping
+    public ResponseEntity<Projeto> cadastrar(@RequestBody Projeto projeto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(projetoService.cadastrarProjeto(projeto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Projeto>> listar(){
+        return ResponseEntity.ok(projetoService.listarProjetos());
+    }
+
+
+}
